154fea1a994852f5be283c80dec3a34ce53bafe7
fielcat <filecat@qq.com>
2024-10-31 14:04:56 +0800

    配置

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 
-const node_word_detection = require("../build/Release/word-detection.node")
+const node_word_detection = require("../build/Release/node-word-detection.node")
 module.exports.node_word_detection = node_word_detection;
