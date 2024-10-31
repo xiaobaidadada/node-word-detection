78572a60d17e83d3eb881c01a61eedcf3b3e54f6
fielcat <filecat@qq.com>
2024-10-31 12:09:15 +0800

    构建测试

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# word-detection
-word detection
+# node-word-detection
+A very memory-efficient lightweight tool for forbidden word detection.
+非常节省内存的轻量级违禁词检测工具
+# Example
+```js
+const {node_word_detection} = require("node-word-detection");
+node_word_detection.add_word("单词1");
+node_word_detection.add_word("word2");
+node_word_detection.add_word("test");
+console.log(node_word_detection.check_word("0h781单词123njn"));
+// print true
+console.log(node_word_detection.check_word("4s3142"));
+// print false
+```
+
+# 说明
+本项目使用c，由于只使用了链表作为hash冲突，添加单词的时候比较慢，如果单词量很多，内存节省会非常多，查询速度上比起其它的项目在50万个单词的测试速度下会比js写的速度快个几毫秒。
--- src/c/main.cc
+++ src/c/main.cc
@@ -1,26 +1,51 @@
 #include "sensitive_struct.h"
 #include <node_api.h>
 #include <napi.h>
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+#include <queue>
+#include <mutex>
+#include <chrono>
+#include <codecvt>
 
 extern p_sensitive_word_node root ;
 
-wchar_t* ConvertNapiStringToWCHAR(const Napi::String& napiStr) {
+typedef struct {
+    const wchar_t *  word;
+    Napi::Promise::Deferred deferred;
+} check_param ,* p_check_param;
+
+std::queue<check_param> queue;
+std::mutex queueMutex;
+std::condition_variable condition; // 可以堵塞线程的条件变量
+// Napi::ThreadSafeFunction tsfn;
+
+// void consumer() {
+//     while (true) {
+//         std::unique_lock<std::mutex> lock(queueMutex);
+//         // 等待期间会释放 unique_lock
+//         condition.wait(lock, [] { return !queue.empty(); }); // 等待直到队列非空
+//
+//         check_param value = queue.front();
+//         queue.pop();
+//         lock.unlock(); // 完成 pop后 释放锁
+//         auto r = check_sensitive_word(value.word);
+//         // 在主线程中处理结果
+//         tsfn.BlockingCall([value,r](Napi::Env env, Napi::Function callback) {
+//             Napi::HandleScope scope(env);
+//             value.deferred.Resolve(Napi::Boolean::New(env, r));
+//         });
+//     }
+// }
+
+const wchar_t * ConvertNapiStringToWCHAR(const Napi::String& napiStr) {
     // 获取 UTF-8 字符串
     std::string utf8Str = napiStr.Utf8Value();
-
-    // 计算宽字符所需的缓冲区大小
-    size_t size = mbstowcs(NULL, utf8Str.c_str(), 0);
-    if (size == (size_t)-1) {
-        return nullptr; // 错误处理
-    }
-
-    // 分配缓冲区
-    wchar_t* wideStr = new wchar_t[size + 1]; // +1 以放置结束符
-
-    // 转换为宽字符
-    mbstowcs(wideStr, utf8Str.c_str(), size + 1);
-
-    return wideStr;
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    std::wstring wideStr = converter.from_bytes(utf8Str);
+    const  wchar_t* nodename = wideStr.c_str();
+    return nodename;
 }
 
 void add_word(const Napi::CallbackInfo &info) {
@@ -32,29 +57,19 @@ void add_word(const Napi::CallbackInfo &info) {
 
 }
 
-Napi::Boolean check_word(const Napi::CallbackInfo &info) {
+Napi::Value check_word(const Napi::CallbackInfo &info) {
     Napi::Env env = info.Env();
     Napi::HandleScope scope(env);
     Napi::String word = info[0].As<Napi::String>();
     auto str = ConvertNapiStringToWCHAR(word);
     auto r = check_sensitive_word(str);
     return Napi::Boolean::New(env, r);
-//     Napi::Promise::Deferred deferred = Napi::Promise::Deferred::New(env);
-//     Napi::ThreadSafeFunction tsfn = Napi::ThreadSafeFunction::New(
-//             env,
-//             deferred.Promise(),  // 传递参数，子线程需要
-//             "ChildThread",                  // 线程名字
-//             0,                              // 从子线程发送到主线程的数据项队列的最大值，0是没有限制
-//             1                              // 最大线程数量，当有这么多个线程调用Release函数后，就会释放了
-//     );
-//     std::thread thread([]() {
-//             // env无法使用
-//             // 子线程执行
-//
-//     });
-//     // 不等待子线程结束
-//     thread.detach();
 
+//     auto str = ConvertNapiStringToWCHAR(word);
+//     check_param param = { str, Napi::Promise::Deferred::New(env) };
+//     queue.push(param);
+//     condition.notify_one(); // 通知消费者
+//     return param.deferred.Promise();
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
@@ -62,6 +77,21 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
     root = (p_sensitive_word_node) malloc(sizeof (sensitive_word_node));
     create_children(root,first_children_length);
     root->children_num = 0;
+//     // 开启线程
+//     std::thread thread([]() {
+//            consumer();
+//     });
+//     // 不等待子线程结束
+//     thread.detach();
+//     // 创建安全的线程函数
+//     Napi::Function callback = Napi::Function::New(env, [](const Napi::CallbackInfo& info) {}); // 提供一个空函数 目的只是使用tsfn来创建线程函数
+//     tsfn = Napi::ThreadSafeFunction::New(
+//                 env,
+//                 callback,  // 传递参数，子线程需要
+//                 "ChildThread",                  // 线程名字
+//                 0,                              // 从子线程发送到主线程的数据项队列的最大值，0是没有限制
+//                 1                              // 最大线程数量，当有这么多个线程调用Release函数后，就会释放了
+//     );
     // 设置函数
     exports.Set(Napi::String::New(env, "add_word"),
                 Napi::Function::New(env, add_word));
