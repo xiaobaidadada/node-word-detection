f6c4bc669e8619b6a7c92f89defe6050f63fbe2f
白保祥 <filecat@qq.com>
2025-02-21 21:57:27 +0800

    README.md

--- test/index_test.js
+++ test/index_test.js
@@ -1,9 +1,12 @@
 
 function abc(){
-    const {node_word_detection} = require("../src/index");
+    const {node_word_detection,get_instance} = require("../src/index");
     node_word_detection.use_sys_sensitive_word();
     console.log(node_word_detection.get_word_num())
     console.log(node_word_detection.find_word("傻逼123",-1));
     console.log(node_word_detection.check_word("傻逼吧"))
+    const abc = get_instance();
+    abc.add_word("123");
+    console.log(abc.check_word("1234"))
 }
 abc()
