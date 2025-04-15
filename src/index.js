cd039072c84b9ab7f2915203b9d63295726bfa7c
白保祥 <filecat@qq.com>
2025-04-15 19:33:37 +0800

    删除prebuilds

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 // const node_word_detection = require("../build/Release/node-word-detection.node")
-var binding = require('node-gyp-build')(path.join(__dirname,'..')) // 自动找到 .node
+// var binding = require('node-gyp-build')(path.join(__dirname,'..')) // 自动找到 .node
+const node_word_detection = require("../build/Release/node-word-detection.node")
+module.exports.node_word_detection = node_word_detection;
 
 function use_sys_sensitive_word (){
     const fs = require("fs");
